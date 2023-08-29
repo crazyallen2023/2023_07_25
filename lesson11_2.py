f0227c34b26a088da264a9f20791c61413b90369
crazyallen2023 <140509830+crazyallen2023@users.noreply.github.com>
2023-08-29 12:33:17 +0000

    321

---
+++ lesson11_2.py
@@ -0,0 +1,25 @@
+import random
+
+def get_score()->list:
+    score = []
+    for i in range(5):
+        score.append(random.randint(50,100))
+    return score
+
+
+def get_names(student_nums):
+    with open('names.txt', encoding='utf-8', newline='') as file:
+        names_str = file.read()
+        names_list = names_str.split(sep='\n')
+        final_list = random.choices(names_list, k=student_nums)
+        return final_list
+
+nums_int = int(input("輸入學生人數："))
+new_list = get_names(nums_int)
+student = []
+for i in range(nums_int):
+    score = get_score()
+    namescore_list = [new_list[i]] + score
+    student.append(namescore_list)
+
+print(student)
